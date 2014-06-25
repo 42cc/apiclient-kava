643e44c1331bc1d0b063a2e3d6559b5971201a26
Yaroslav Klyuyev <imposeren@gmail.com>
2014-06-25 15:15:05 +0300

    fixed tests

--- tests/general_workflow_tests.py
+++ tests/general_workflow_tests.py
@@ -20,40 +20,64 @@ class GeneralWorkflowTest(unittest2.TestCase):
         project_slug = '42-jobs'
         project_api = api.projects.get(project_slug)
 
+        response_mock = mock.MagicMock()
+        response_mock.status_code = 200
+        requests.get.return_value = response_mock
+        requests.post.return_value = response_mock
+
         project_api.response
-        requests.get.assert_called_with(
-            urlparse.urljoin(api.base_url, 'project/%s/' % project_slug),
-            params={'project_slug': project_slug},
-            auth=auth,
+        self.assertEqual(
+            requests.get.call_args[0][0],
+            urlparse.urljoin(api.base_url, 'project/%s/' % project_slug)
+        )
+        self.assertDictContainsSubset(
+            {
+                'project_slug': project_slug,
+                'project': project_slug,
+            },
+            requests.get.call_args[1]['params'],
         )
 
         project_api.estimate.response
-        requests.get.assert_called_with(
-            urlparse.urljoin(api.base_url, 'project/estimate/'),
-            params={
+        self.assertEqual(
+            requests.get.call_args[0][0],
+            urlparse.urljoin(api.base_url, 'project/estimate/')
+        )
+        self.assertDictContainsSubset(
+            {
                 'project_slug': project_slug,
+                'project': project_slug,
                 'company': company_name,
-            }
+            },
+            requests.get.call_args[1]['params'],
         )
 
         project_api.properties.response
-        requests.post.assert_called_with(
-            urlparse.urljoin(api.base_url, 'project/properties/'),
-            data={
+        self.assertEqual(
+            requests.post.call_args[0][0],
+            urlparse.urljoin(api.base_url, 'project/properties/')
+        )
+        self.assertDictContainsSubset(
+            {
                 'project_slug': project_slug,
+                'project': project_slug,
                 'company': company_name,
             },
-            auth=auth,
+            requests.post.call_args[1]['data'],
         )
 
         project_api.tickets.response
-        requests.post.assert_called_with(
-            urlparse.urljoin(api.base_url, 'project/tickets/'),
-            data={
+        self.assertEqual(
+            requests.post.call_args[0][0],
+            urlparse.urljoin(api.base_url, 'project/tickets/')
+        )
+        self.assertDictContainsSubset(
+            {
                 'project_slug': project_slug,
+                'project': project_slug,
                 'company': company_name,
             },
-            auth=auth,
+            requests.post.call_args[1]['data'],
         )
 
         api.projects.response
